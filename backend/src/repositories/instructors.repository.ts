8afce2b50ffc946d6a0f162bec4663355d774dd0
Mario <mane.sto@outlook.com>
2022-08-17 16:12:51 +0200

    implented get for all APIs

--- backend/src/repositories/instructors.repository.ts
+++ backend/src/repositories/instructors.repository.ts
@@ -2,13 +2,13 @@ import { pgsql } from "../config/db";
 
 async function findAll(): Promise<any> {
   return await 
-    pgsql("course")
+    pgsql("instructor")
     .select("*");
 }
 
 async function findById(id: number): Promise<any> {
   return await 
-    pgsql("course")
+    pgsql("instructor")
     .where("id", id)
 }
 
